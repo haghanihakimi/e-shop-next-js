ceeb7840513ddfe2c823a5076077cb8910eba223
Haghani Hakimi <61259672+haghanihakimi@users.noreply.github.com>
2023-09-24 19:09:47 +1000

    all-orders api "Cannot set headers after they are sent to the client" error fixed.

--- src/pages/api/orders/all-orders/index.ts
+++ src/pages/api/orders/all-orders/index.ts
@@ -3,42 +3,38 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { PrismaClient } from '@prisma/client';
 
-import { getRateLimitMiddlewares } from '@/ratelimit';
-import applyMiddleware from "@/ratelimit";
-import applyRateLimit from '@/ratelimit';
-
 const prisma = new PrismaClient();
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-
     if (req.method === 'GET') {
         const session = await getServerSession(req, res, authOptions);
 
         if (!session) {
             return res.status(401).json("Unauthorized access!");
         }
+
         try {
-            await prisma.$transaction(async (prisma) => {
-                const allOrders = await prisma.order.findMany({
-                    where: {
-                        userId: session?.user?.id,
-                    },
-                    include: {
-                        items: true,
-                    }
-                });
-                if(allOrders && allOrders.length > 0) {
-                    return res.status(200).json(allOrders);
+            const allOrders = await prisma.order.findMany({
+                where: {
+                    userId: session?.user?.id,
+                },
+                include: {
+                    items: true,
                 }
             });
-            return res.status(200).json([]);
+
+            if (allOrders && allOrders.length > 0) {
+                return res.status(200).json(allOrders);
+            } else {
+                // return res.status(200).json([]);
+            }
         } catch (e) {
-            return res.status(500).json({ error: "Unable to place order at this time. Please check your payment details and try again. 500" });
+            // console.error(e);
+            return res.status(500).json({ error: "Unable to fetch orders at this time." });
         } finally {
             await prisma.$disconnect();
         }
-
     } else {
-        return res.status(405).end("Internal server error.");
+        return res.status(405).end("Method Not Allowed");
     }
 }
